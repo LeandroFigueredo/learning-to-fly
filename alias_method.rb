3b1fa14c66e8986911f239807161a967c79e665e
= <=>
2017-02-02 08:39:24 -0200

    Play with alias method in ruby

---
+++ alias_method.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'pp'
+# Today, we'll learn to use this special command trick, the alias method.
+
+# With that, we can change name of the methods in diferent classes, calling the exact same method
+
+class Alias
+  def hello
+    puts "Hello from original =========> Aqui o método original"
+  end
+
+  alias :new_hello :hello
+
+  def new_hello
+    puts "Aqui é um método novo  ===========> Inicio do método alias"
+    hello
+    puts "Aqui eu printo a saída"
+  end
+end
+
+
+pp Alias.new.new_hello
+
--- class_eval.rb
+++ class_eval.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+
+#define class Hello
 class Hello
 end
 
